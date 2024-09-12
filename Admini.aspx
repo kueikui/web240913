001d48460dc3ac34b692b59225ec2ccf8c54ecb9
kueikui <130137975+kueikui@users.noreply.github.com>
2024-09-13 02:13:28 +0800

    Add files via upload

---
+++ Admini.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="Admini.aspx.cs" Inherits="web_1.Web.Admini" %>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+     <style>
+         body {
+            background-image: url(../Images/b-4.jpg);
+            background-size:cover;
+         }
+         .panel-common{
+             border-radius: 10px;
+             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+             padding:10px;
+         }
+        
+     </style>
+
+
+
+    <main aria-labelledby="title">
+         <h1>管理員資料</h1>
+
+  <asp:Panel ID="Panel2" runat="server" BackColor="PowderBlue" CssClass="panel-common"> <!--BackColor="#FFFFCC"-->
+      管理員資料<br /> 管理員編號:&nbsp; &nbsp;
+      <asp:Label ID="cId_Text" runat="server"></asp:Label>
+      <br />
+      管理員姓名:<asp:TextBox ID="cName_Text" runat="server" Width="160px" ReadOnly="True"></asp:TextBox>
+      &nbsp;身分證字號:<asp:TextBox ID="cIdCard_Text" runat="server" Width="160px" ReadOnly="True"></asp:TextBox>
+      &nbsp;性別:&nbsp;&nbsp;<asp:DropDownList ID="cGender_list" runat="server" ReadOnly="True" Enabled="False">
+          <asp:ListItem>male</asp:ListItem>
+          <asp:ListItem>female</asp:ListItem>
+      </asp:DropDownList>
+      &nbsp;生日:<asp:TextBox ID="cBirth_Text" runat="server" Width="160px" TextMode="Date" ReadOnly="True"></asp:TextBox>
+      <br />
+      電話:<asp:TextBox ID="cPhone_Text" runat="server" Width="160px" ReadOnly="True"></asp:TextBox>
+      &nbsp;通訊地址:
+      <asp:TextBox ID="cAddress_Text" runat="server" Width="200px" ReadOnly="True"></asp:TextBox>
+      &nbsp;<br />
+      <br />
+  </asp:Panel>
+        <Br />
+  <asp:Panel ID="Panel3" runat="server" BackColor="powderblue" CssClass="panel-common"><!--BackColor="#99FFCC"-->
+      負責的長者<br /> 
+      <asp:GridView ID="GridView2" runat="server">
+      </asp:GridView>
+      <br /> 
+     
+      <asp:Button ID="edit" runat="server" OnClick="edit_Click1" Text="編輯" />
+     
+      <asp:Button ID="save" runat="server" Text="儲存" OnClick="save_Click" Visible="False" />
+      <asp:Button ID="changePW" runat="server" OnClick="changePW_Click" Text="更改密碼" />
+  </asp:Panel>
+    </main>
+</asp:Content>
---
+++ Admini.aspx.designer.cs
@@ -0,0 +1,134 @@
+﻿//------------------------------------------------------------------------------
+// <自動產生>
+//     這段程式碼是由工具產生的。
+//
+//     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
+//     程式碼，則會遺失變更。
+// </自動產生>
+//------------------------------------------------------------------------------
+
+namespace web_1.Web
+{
+
+
+    public partial class Admini
+    {
+
+        /// <summary>
+        /// Panel2 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel2;
+
+        /// <summary>
+        /// cId_Text 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label cId_Text;
+
+        /// <summary>
+        /// cName_Text 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cName_Text;
+
+        /// <summary>
+        /// cIdCard_Text 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cIdCard_Text;
+
+        /// <summary>
+        /// cGender_list 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList cGender_list;
+
+        /// <summary>
+        /// cBirth_Text 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cBirth_Text;
+
+        /// <summary>
+        /// cPhone_Text 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cPhone_Text;
+
+        /// <summary>
+        /// cAddress_Text 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox cAddress_Text;
+
+        /// <summary>
+        /// Panel3 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel3;
+
+        /// <summary>
+        /// GridView2 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView2;
+
+        /// <summary>
+        /// edit 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button edit;
+
+        /// <summary>
+        /// save 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button save;
+
+        /// <summary>
+        /// changePW 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button changePW;
+    }
+}
